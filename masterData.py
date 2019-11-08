f53fe45133cb4efe51657b6041ff22f8cd4c1af8
ahalarewicz <alhalarewicz@gmail.com>
2019-11-07 19:09:55 -0800

    computing n-day rolling  avg columns

--- masterData.py
+++ masterData.py
@@ -2,6 +2,7 @@ import pandas as pd
 print('pandas imported as pd')
 import datetime
 print('datetime imported')
+import matplotlib.pyplot as plt
 
 # Date and time of file execution
 now = datetime.datetime.now()
@@ -32,8 +33,33 @@ master_df.weekday = master_df.weekday.dt.day_name()
 #master_df['weekday'] = pd.to_Date
 print(master_df.head())
 
+# Add daily avg ticket column
+print('Adding Avg Daily Ticket column')
+master_df['avgTkt'] = master_df.total / master_df.ticketsSold
+master_df.avgTkt = master_df.avgTkt.round(2)
+print(master_df.head())
+
+# Compute rolling averages (7, 14, 30, 60, 90)
+print('Creating moving average columns for 7D, 14D, 30D, 60D, & 90D windows')
+def rolling_avg(n):
+    """returns a column for  a moving n-day average of master_df """
+    return master_df.total.rolling(window=n).mean()
+master_df['7Day'] = rolling_avg(7)
+master_df['14Day'] = rolling_avg(14)
+master_df['30Day'] = rolling_avg(30)
+master_df['60Day'] = rolling_avg(60)
+master_df['90Day'] = rolling_avg(90)
+print(master_df.head(100))
+
+# Plot Totals for each day
+master_df['total'].plot()
+plt.show()
+
 # Write data to excelfile
 print('writing data to ' + OUTPUT_FILE)
 master_df.to_csv(OUTPUT_FILE)
 
+
 print('DONE')
+
+
